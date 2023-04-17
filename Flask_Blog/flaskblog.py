a82336caf0ed09373501fb535d2a23b9b9efb767
TarakaKoda <taraka.koda@gmail.com>
2023-04-17 19:16:23 +0530

    First web application

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,2 +1,11 @@
-def first_web_applications():
-    pass
+from flask import Flask
+
+app = Flask(__name__)  # app is an instance of WSGI (Web Server Gateway Interface)
+                       # __name__ is a name of the application’s module or package (eg. flaskblog)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
