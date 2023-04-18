810ac6182254879d32742df215230887bccad366
TarakaKoda <taraka.koda@gmail.com>
2023-04-18 18:51:20 +0530

    added custom templates for flask blog

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,26 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)    # app is a WSGI application (Web Server Gateway Interface)
                          # __name__ is a name of the application’s module or package.
 
 
+posts = [
+    {
+        "author": "Srinivasu Koda",
+        "title": "Blog Post 1",
+        "content": "This is my first post",
+        "date": "June 30, 2023",
+        "contact": 906910000
+    },
+    {
+        "author": "Naga Pavan",
+        "title": "Blog Post 2",
+        "content": "This is my second post",
+        "date": "July 23, 2023",
+        "contact": 9398809883
+    }
+]
+
+
 @app.route("/")
 @app.route("/home")
-def home_page():
-    return f"<h1>Home Page</h1>" \
-           f"<h1>Welcome to our home page</h1>"
+def home():
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
-def about_page():
-    return f"<h1>This is our about page</h1>"
-
-
-@app.route("/login")
-def login_page():
-    return f"<h1>This is our Login Page</h1>"
+def about():
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
