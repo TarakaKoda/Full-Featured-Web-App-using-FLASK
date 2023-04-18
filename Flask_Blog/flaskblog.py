becff9e6e4f89536e41213c935bc7281832cd35a
TarakaKoda <taraka.koda@gmail.com>
2023-04-18 15:21:56 +0530

    added additional route() - login

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,8 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask(__name__)    # app is a WSGI application (Web Server Gateway Interface)
+                         # __name__ is a name of the application’s module or package.
 
 
 @app.route("/")
+@app.route("/home")
 def home_page():
     return f"<h1>Home Page</h1>" \
            f"<h1>Welcome to our home page</h1>"
@@ -11,6 +14,11 @@ def home_page():
 @app.route("/about")
 def about_page():
     return f"<h1>This is our about page</h1>"
+
+
+@app.route("/login")
+def login_page():
+    return f"<h1>This is our Login Page</h1>"
 
 
 if __name__ == "__main__":
