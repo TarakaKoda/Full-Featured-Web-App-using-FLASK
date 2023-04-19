5d741819007b93589c250aafd8242a65826e2e09
TarakaKoda <taraka.koda@gmail.com>
2023-04-19 23:24:01 +0530

    added login and register templates.

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)    # app is a WSGI application (Web Server Gateway Interface)
                          # __name__ is a name of the application’s module or package.
+
+app.config["SECRET_KEY"] = "1de386d816c4c0484ffa4e3dc43f21ea"  # to get a good set of random characters
 
 
 posts = [
@@ -31,6 +34,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", tilte="Login", form=form)
 
 
 if __name__ == "__main__":
