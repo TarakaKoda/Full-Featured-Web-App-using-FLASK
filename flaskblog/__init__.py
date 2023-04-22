120edfe9c4fcaad6a0daf27dec4c7e40212f7a40
TarakaKoda <taraka.koda@gmail.com>
2023-04-22 23:28:33 +0530

    restructured package flaskblog

---
+++ flaskblog/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)  # app is a WSGI application (Web Server Gateway Interface)
+# __name__ is a name of the application’s module or package.
+
+app.config["SECRET_KEY"] = "1de386d816c4c0484ffa4e3dc43f21ea"  # this is used for protection from the treats.
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+from flaskblog import routes
+
