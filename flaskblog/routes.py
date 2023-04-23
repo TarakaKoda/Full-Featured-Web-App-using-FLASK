e069f1cd1a41e78643a692edf3df82cf315cb2b4
TarakaKoda <taraka.koda@gmail.com>
2023-04-24 00:07:37 +0530

    changed flash message of register route: from "login" to "log in"

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -42,7 +42,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash(f"Your account has been created! You are now able to login", "success")
+        flash(f"Your account has been created! You are now able to log in", "success")
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
