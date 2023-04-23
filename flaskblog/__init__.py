b2b6dd371e196e317f196220875de8b0a2fa5c32
TarakaKoda <taraka.koda@gmail.com>
2023-04-23 22:47:14 +0530

    created instances of flask_bcrypt, flask_login

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)  # app is a WSGI application (Web Server Gateway Interface)
 # __name__ is a name of the application’s module or package.
@@ -7,6 +9,9 @@ app = Flask(__name__)  # app is a WSGI application (Web Server Gateway Interface
 app.config["SECRET_KEY"] = "1de386d816c4c0484ffa4e3dc43f21ea"  # this is used for protection from the treats.
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = 'warning'
 
 from flaskblog import routes
-
