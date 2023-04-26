a9f6764c1e6855f0cb44de5676c786c7afbb96fd
TarakaKoda <taraka.koda@gmail.com>
2023-04-26 14:23:44 +0530

    added mail,server, port, mail user info

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)  # app is a WSGI application (Web Server Gateway Interface)
 # __name__ is a name of the application’s module or package.
@@ -13,5 +15,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = 'warning'
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USER_TLS"] = False
+app.config["MAIL_USER_SSL"] = True
+app.config["MAIL_USERNAME"] = "srinivaskoda9@gmil.com"
+app.config["MAIL_PASSWORD"] = "sunrisers"
+mail = Mail(app)
 
 from flaskblog import routes
