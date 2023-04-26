db4655bc48a7b5b70a1b9b63cc1a9e40cd247802
TarakaKoda <taraka.koda@gmail.com>
2023-04-27 01:11:46 +0530

    created a config file

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+class Config:
+    SECRET_KEY = "1de386d816c4c0484ffa4e3dc43f21ea"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 465
+    MAIL_USER_TLS = False
+    MAIL_USER_SSL = True
+    MAIL_USERNAME = "youremail@demo.com"
+    MAIL_PASSWORD = "yourpassword"
+
